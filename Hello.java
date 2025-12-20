3e83124358bc68b1bfe8637eb9cfe4bf6fa7ebe4
Jassayy <jasgandhi2004@gmail.com>
2025-12-20 15:15:45 +0530

    methods

--- Hello.java
+++ Hello.java
@@ -1,36 +1,24 @@
-class Hello{
-       int instVar = 10;
-
-       static String s = "I am statuc";
-
-       //method
-       public void showVar(){
-              int localVar = 50;
-
-              System.out.println("Ins var - " + instVar);
-              System.out.println("static var - " + s);
-              System.out.println("Local var - " + localVar);
-
+//methods
+class Computer {
+       public void playMusic() {
+              System.out.println("Music playing");
        }
-       public static void main(String[] arg){
-             int a = 5;
-             int b = 10;
-             System.out.println(a<b); //prints true or false
 
-             //if else statements remain the same
-             //switch statements
-             int day = 9;
-             switch (day) {
-              case 0:
-                    System.out.println(0); 
-                     break;
-              case 3:
-                     System.out.println("Hello bro");
-                     break;
-             
-              default:
-                     System.out.println("ew");
-                     break;
-             }
+       public String getPen(int cost) {
+              if (cost > 10) {
+
+                     return "pen";
+              } else {
+                     return "Chiggi boy";
+              }
+       }
+}
+
+public class Hello {
+       public static void main(String[] arg) {
+              Computer obj = new Computer();
+              String s = obj.getPen(15);
+              System.out.println(s);
+              obj.playMusic();
        }
 }
