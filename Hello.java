e5cb236bf4a7cd0ba12537fdf0350141d1ac86ee
Jassayy <jasgandhi2004@gmail.com>
2025-12-20 16:28:06 +0530

    stack heap

--- Hello.java
+++ Hello.java
@@ -1,12 +1,17 @@
 //method overloading
 
 class Calculator {
+       int num = 5; //instance variable
        public int addTwoNum(int a, int b) {
               return a + b;
        }
 
        public int addTwoNum(int a, int b, int c) {
               return a + b + c;
+       }
+
+       public double addTwoNum(double a , int b){ //can contain different data type as well
+              return a+b;
        }
 
 }
@@ -18,6 +23,14 @@ public class Hello {
               System.out.println(res);
               int resu = obj.addTwoNum(1, 2, 3);
               System.out.println(resu);
+              double result = obj.addTwoNum(1.2, 6);
+              System.out.println(result);
+              Calculator obj1 = new Calculator();
+              obj1.num = 30;
+              System.out.println(obj1.num); //each object has its own copy of inst variable
+              System.out.println(obj.num);
+              //but for static variables
+              //they are shared with all objects and a change in any object will refelct in all objects
 
        }
 }
