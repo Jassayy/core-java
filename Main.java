585ab641e2b4204b9d0d9e747b9baaa15d9125c8
Jassayy <jasgandhi2004@gmail.com>
2026-01-05 13:23:40 +0530

    threads

--- Main.java
+++ Main.java
@@ -1,40 +1,51 @@
-public class Main {
+//threads in java -> the smallest unit of execution within a process that can be scheduled by the operating system
+//runs parallel to each other
 
-       public static void main(String[] args) {
-              int i = 0, j = 0;
-              int[] arr = new int[5];
+//multiple threads
 
-              try {
-
-                     if (i == 0) {
-                            throw new MyException("Cannot divide by zero - custom exception");
-                     }
-                     j = 18 / i;
-
-                     if (j == 0) {
-                            throw new MyException("This is customised exception");
-                     }
-
-                     System.out.println(arr[5]);
-
-              } catch (MyException e) {
-                     // TODO: handle exception
-                     System.out.println(e); // gives exception
-
-                     // array index out of bounds exception
-                     // arithmetic exceptions
-                     // we can have customized exceptions as well
-              }
-
-              System.out.println("Outside try catch block");
-       }
+class A extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hi");
+        }
+    }
 }
 
-class MyException extends Exception { // we need to extend to exception as it needs to be a subclass of throwable
-       public MyException(String s) {
-              // System.out.println(s); //this doesnt work
-              //to print we need to call constructor of superclass as it is handled by the exception class
-              super(s);
-       }
+class B extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+
+// when we do extends Thread...now they are not normal classes they are threads
+// but still the process executes sequentially
+// for that we use .start() method to start execution of the thread
+
+public class Main
+
+{
+
+    public static void main(String args[])
+
+    {
+        A obj = new A();
+        B obj1 = new B();
+
+        // obj.show();
+        // obj1.show();
+
+        obj.start(); // change show() -> run() as start calls run
+        obj1.start();
+    }
 
 }
+
+//BY DOING THIS
+//these threads run simultaneously but the output is kinda 
+//hi
+//hi
+//hello
+//hello
+//etc etc
