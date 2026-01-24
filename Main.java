03a5ea43f97f56f43c92c9ccf86fa2371378f339
Jassayy <jasgandhi2004@gmail.com>
2026-01-24 15:31:08 +0530

    localdate

--- Main.java
+++ Main.java
@@ -1,58 +1,34 @@
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
+
     public static void main(String[] args) {
-        LocalDate date = LocalDate.now();
 
-        System.out.println(date);
+        LocalDate manufacturedDate = LocalDate.of(2026, Month.JANUARY, 24);
 
-        LocalDate date2 = LocalDate.of(2045, Month.FEBRUARY, 19);
+        System.out.println("Manufactirung Date : " + manufacturedDate);
+        // calculate expiry date
+        // check whether product is expired valid or expiring soon(within 30 days)
+        int expiredPeriodInMonths = 6;
 
-        System.out.println(date2);
+        LocalDate expiryDate = manufacturedDate.plusMonths(expiredPeriodInMonths);
 
-        LocalDate date3 = LocalDate.parse("2026-12-03");
-        System.out.println(date3);
+        System.out.println("Expiry Date : " + expiryDate);
 
-        LocalDate date4 = LocalDate.now();
+        if ((LocalDate.now()).isAfter(expiryDate)) {
+            System.out.println("Expired");
+        } else if (ChronoUnit.DAYS.between(LocalDate.now(), expiryDate) <= 30) {
+            System.out.println("Is Expiring soon");
+        } else {
+            System.out.println("Valid");
 
-        System.out.println(date4.toString() + " " + date4.hashCode());
-        date4 = date4.plusDays(5);
-        System.out.println(date4.toString() + " " + date4.hashCode()); // both hashcodes are different
+        }
 
-        // get values from local date
-        LocalDate date5 = LocalDate.now();
-        System.out.println(date5.getYear()); // 2026
-        System.out.println(date5.getMonth()); // january
-        System.out.println(date5.getMonthValue()); // 1
-        System.out.println(date.getDayOfMonth()); // 24
-        System.out.println(date5.getDayOfYear()); // 24
-        System.out.println(date5.getDayOfWeek()); // saturday
+        LocalDate d1 = LocalDate.now();
+        LocalDate d2 = d1.plusDays(7);
 
-        LocalDate date6 = LocalDate.of(2026, Month.JANUARY, 30);
-
-        date6 = date6.plusDays(5);
-        System.out.println(date6);
-        date6 = date6.plusMonths(1);
-        System.out.println(date6);
-        date6 = date6.plusYears(4);
-        System.out.println(date6);
-
-        // isBefore isAfter
-        System.out.println(date6.isAfter(date5));
-
-        // date time formatter
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        String dStr = "2026-04-06";
-
-        LocalDate date7 = LocalDate.parse(dStr, formatter);
-        System.out.println(date7.getClass());
-        System.out.println(date7.getEra());
-
-        String datestr = date7.toString();
-        System.out.println(datestr);
-
+        System.out.println(ChronoUnit.DAYS.between(d1, d2));
     }
 }
